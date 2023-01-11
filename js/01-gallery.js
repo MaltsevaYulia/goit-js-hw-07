e6b283fafa9f2d086f5eead6b6c7a705b5b8f8af
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-01-11 11:38:55 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galaryContainer=document.querySelector('.gallery')
+const galaryMarkup = createGalaryMarkup(galleryItems);
+galaryContainer.insertAdjacentHTML('beforeend',galaryMarkup)
+
+function createGalaryMarkup(galleryItems) {
+    return galleryItems.map(({preview,original,description}) =>
+        `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>
+        `
+    ).join('')
+
+}
+
+galaryContainer.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+    console.log(evt.target);
+}
